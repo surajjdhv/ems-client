f9ab83fb882e3a73a9e53e26176f0af653a8744f
Akash Mahadik <akashpm1996@gmail.com>
2024-06-09 03:13:42 +0530

    Changes: - Remove initial template files - Add tailwindcss support - Add and update login page from TailAdmin template

---
+++ src/components/Auths/DefaultAuthCard.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+const props = defineProps(['subtitle', 'title', 'customClasses'])
+</script>
+
+<template>
+  <!-- ====== Forms Section Start -->
+  <div class="min-h-screen flex flex-wrap items-center bg-white dark:bg-boxdark">
+    <div class="hidden w-full md:block md:w-1/2">
+      <div class="md:p-12.5 xl:p-17.5 text-center">
+        <router-link class="mb-5.5 inline-block" to="/">
+          <img class="hidden dark:block" src="@/assets/images/logo/logo.svg" alt="Logo" />
+          <img class="dark:hidden" src="@/assets/images/logo/logo-dark.svg" alt="Logo" />
+        </router-link>
+
+        <p class="font-medium 2xl:px-20">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit suspendisse.
+        </p>
+
+        <span class="mt-15 inline-block">
+          <img src="@/assets/images/illustration/login-illustration.svg" alt="illustration" />
+        </span>
+      </div>
+    </div>
+    <div class="w-full border-stroke dark:border-strokedark md:w-1/2 md:border-l-2">
+      <div class="w-full p-4 sm:p-12.5 xl:p-17.5">
+        <span class="mb-1.5 block font-medium">{{ props.subtitle }}</span>
+        <h2 class="mb-9 text-2xl font-bold text-black dark:text-white sm:text-title-xl2">
+          {{ props.title }}
+        </h2>
+
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+  <!-- ====== Forms Section End -->
+</template>
